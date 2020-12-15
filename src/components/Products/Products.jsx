f448e428fe4a4213d5c2b92084ea7ec6fadd7c63
Zahra <zbehfarshad@gmail.com>
2020-12-15 04:15:11 -0800

    Displaying the products as grid in Products component and adding the component to App.js

---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Grid } from '@material-ui/core';
+
+const products = [
+    {
+        id: '1',
+        name: 'tv',
+        description: 'Samsung TV 42 inch'
+    },
+    {
+        id: '2',
+        name: 'laptop',
+        description: 'Dell Laptop'
+    }
+];
+
+const Products = () => {
+    return (
+        <main>
+            <Grid container justify='center' spacing={4}>
+                { products.map(product => {
+                    <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>{product.name}</Grid>
+                })}
+            </Grid>
+        </main>
+    )
+}
+
+export default Products;
