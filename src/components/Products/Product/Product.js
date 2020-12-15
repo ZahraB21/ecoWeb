7f51154db1491149b0d521bb21c7a342e3cda15d
Zahra <zbehfarshad@gmail.com>
2020-12-15 04:28:23 -0800

    |

---
+++ src/components/Products/Product/Product.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Card, CardMedia, CardContent, CardActions, Typography, IconButton, Icon} from '@material-ui/core';
+import { AddShoppingCart } from '@material-ui/icons';
+
+const Product = ({ product }) => {
+    return (
+       <Card className={classes.root}>
+        <CardMedia className={classes.media} image='' title={product.name} />
+        <CardContent>
+            <div className={classes.cardContent}>
+                <Typography varient='h5' gutterBottom>
+                    {product.name}
+                </Typography>
+                <Typography varient='h5'>
+                    {product.price}
+                </Typography>
+            </div>
+            <Typography varient='h2' color='textSecondary'>
+                {product.description}
+            </Typography>
+        </CardContent>
+        <CardActions disableSpacing className={classes.cardActions}>
+            <IconButton>
+                <AddShoppingCart />
+            </IconButton>
+        </CardActions>
+       </Card>
+    )
+}
+
+export default Product
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
 
+import Product from './Product/Product';
+
 const products = [
     {
         id: '1',
         name: 'tv',
-        description: 'Samsung TV 42 inch'
+        description: 'Samsung TV 42 inch',
+        price: '$500'
     },
     {
         id: '2',
         name: 'laptop',
-        description: 'Dell Laptop'
+        description: 'Dell Laptop',
+        price: '$1000'
     }
 ];
 
@@ -19,7 +23,9 @@ const Products = () => {
         <main>
             <Grid container justify='center' spacing={4}>
                 { products.map(product => {
-                    <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>{product.name}</Grid>
+                    <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+                        <Product product={product} />
+                    </Grid>
                 })}
             </Grid>
         </main>
