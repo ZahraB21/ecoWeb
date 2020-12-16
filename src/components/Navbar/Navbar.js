b7691ab2994d07e36c16c2d18e2d7c7e50264e65
Zahra <zbehfarshad@gmail.com>
2020-12-16 14:55:28 -0800

    Creating assets folder and adding logo image and to Navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core';
 import { AddShoppingCart } from '@material-ui/icons';   
 
+import logo from '../../assets/images/logo';
+
 const Navbar = () => {
     return (
         <>
             <AppBar postion="fixed" color = "inherit" className = { classes.appBar}>
                 <Toolbar>
                     <Typography varient = "h6" className = {classes.title} color = "inherit">
-                        <img src="" alt = "Ecommerce Website" height = "25px" />
+                        <img src={logo} alt = "Ecommerce Website" height = "25px" />
                         EcoWeb
                     </Typography>
                 </Toolbar>
