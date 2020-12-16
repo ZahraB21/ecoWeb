994f484310d2b2e3a12b65000a13303a6e1cc8d7
Zahra <zbehfarshad@gmail.com>
2020-12-16 15:22:46 -0800

    Creating the styles for the Products.jsx and importing it into it and apply necessary styles

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Grid } from '@material-ui/core';
 
 import Product from './Product/Product';
+import useStyles from './styles';
 
 const products = [
     {
@@ -21,8 +22,10 @@ const products = [
 ];
 
 const Products = () => {
+    const classes = useStyles();
     return (
-        <main>
+        <main className = {classes.content}>
+            <div className = {classes.toolbar} />
             <Grid container justify='center' spacing={4}>
                 { products.map( (product) => {
                     return (
