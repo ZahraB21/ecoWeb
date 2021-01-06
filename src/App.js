c7be3337df10ad1d7ad6a2d0a471aea83ec2640d
Zahra <zbehfarshad@gmail.com>
2021-01-06 15:40:06 -0800

    Defining cart state and passing handleAddToCart as props to Product to add items to cart if cart is clicked

--- src/App.js
+++ src/App.js
@@ -4,21 +4,34 @@ import { Navbar, Products } from './components';
 
 const App = () => {
     const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState({});
     
     const fetchProducts = async () => {
         const { data } = await commerce.products.list();
         setProducts(data);
     }
 
+    const fetchCart = async () => {
+        const cart = await commerce.cart.retrieve();
+        setCart(cart);
+    }
+
+    const handleAddToCart = async (productId, quantity) => {
+        const items = await commerce.cart.add(productId, quantity);
+
+        setCart(items);
+    }
+
     useEffect(() => {
         fetchProducts();
+        fetchCart();
     },[])
 
-    console.log(products);
+    console.log(cart);
     return (
         <div>
             <Navbar />
-            <Products products={products} />
+            <Products products={products} onAddToCart={handleAddToCart} />
         </div>
     )
 }
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -21,7 +21,7 @@ const products = [
     }
 ];
 
-const Products = ({ products }) => {
+const Products = ({ products, onAddToCart }) => {
     const classes = useStyles();
     return (
         <main className = {classes.content}>
@@ -30,7 +30,7 @@ const Products = ({ products }) => {
                 { products.map( (product) => {
                     return (
                         <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-                            <Product product={product} />
+                            <Product product={product} onAddToCart={onAddToCart}/>
                         </Grid>
                     );
                 })}
