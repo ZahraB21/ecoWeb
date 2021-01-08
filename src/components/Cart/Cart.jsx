4fa4595ea2ccd91a833cc7b7a1ab6aabd0843edb
Zahra <zbehfarshad@gmail.com>
2021-01-07 17:06:01 -0800

    Checking if the cart is empty or items have been added

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,10 +2,21 @@ import React from 'react';
 import { Container, Typography, Button, Grid } from '@material-ui/core';
 
 const Cart = () => {
+    const isEmpty = true;
+
+    const EmptyCart = () => {
+        <Typography variant = "subtitle1">You have no items in your shopping cart, start adding some!</Typography>
+    }
+
+    const FilledCart = () => {
+        
+    }
+
     return (
         <Container>
             <div className={classes.toolbar}>
                 <Typography className={classes.title}>Your Shopping Cart</Typography>
+                { isEmpty ? <EmptyCart /> : <FilledCart />}
             </div>
         </Container>
     )
