541d39527fcfc43c0a00e6210b73f9559aa3f4ef
Zahra <zbehfarshad@gmail.com>
2021-01-12 16:15:50 -0800

    Displaying the Cart Items in a grid style

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -11,7 +11,10 @@ const Cart = ( { cart }) => {
     const FilledCart = () => {
         <>
             <Grid container spacing ={3}>
-
+                {cart.line_items.map(item => 
+                <Grid item xs={12} sm={4} key={item.id}>
+                    <div>{item.name}</div>
+                </Grid>)}
             </Grid>
         </>
     }
