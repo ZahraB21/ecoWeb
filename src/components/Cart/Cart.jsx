e2a8062d2513eef06cc8a4a1809c32bd7a0c1bdd
Zahra <zbehfarshad@gmail.com>
2021-01-14 09:34:07 -0800

    Definning the styles.js and importing it in the Cart component

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Container, Typography, Button, Grid } from '@material-ui/core';
 
+import useStyles from './styles';
+
 const Cart = ( { cart }) => {
     const isEmpty = !cart.line_items.length;
 
