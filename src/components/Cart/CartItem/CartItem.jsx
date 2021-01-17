2a4919881ebfc3d6b27ac01846039bda23879a63
Zahra <zbehfarshad@gmail.com>
2021-01-16 22:29:34 -0800

    Importing necessary items from material-ui

---
+++ src/components/Cart/CartItem/CartItem.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Typography, Card, CartAction, CardContent, CardMedia, Button} from '@material-ui/core';
+
+const CartItem = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default CartItem
